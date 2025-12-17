419dcb60e770244cef4e812c95a35920296198f2
Thomas Neyman <neymanthomas45@gmail.com>
2025-12-17 15:50:53 -0700

    Added the ability to add custom items

---
+++ src/main/java/net/thomas/zomsmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,33 @@
+package net.thomas.zomsmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.thomas.zomsmod.ZomsMod;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ZomsMod.MOD_ID);
+
+    //CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.KRABBY_PATTY.get() sets the icon for the creative tab
+    public static final RegistryObject<CreativeModeTab> ZOMS_TAB = CREATIVE_MODE_TABS.register("zoms_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.KRABBY_PATTY.get()))
+                    .title(Component.translatable("creativetab.zoms_mod"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.KRABBY_PATTY.get());
+                        output.accept(ModItems.LETTUCE.get());
+
+                        // you can also add Vanilla items to the list
+                        output.accept(Items.DIAMOND);
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/thomas/zomsmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.thomas.zomsmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.thomas.zomsmod.ZomsMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, ZomsMod.MOD_ID);
+
+    public static final RegistryObject<Item> KRABBY_PATTY = ITEMS.register("krabby_patty", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LETTUCE = ITEMS.register("lettuce", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
